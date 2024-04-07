98dddea58b4534e152d10b449876e2a6f4bf76d5
Mccortez <marcelocarlini@hotmail.com>
2024-04-07 05:31:43 -0300

    concluido backend de vinculação

--- src/components/produto/ProdutoVincular.js
+++ src/components/produto/ProdutoVincular.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Card, CardContent, FormControl, InputLabel, MenuItem, Select, TextField, Box, Table, TableContainer, TableHead, TableBody, TableRow, TableCell, Paper } from '@mui/material';
+import { Card, CardContent, FormControl, InputLabel, MenuItem, Select, TextField, Box, Table, TableContainer, TableHead, TableBody, TableRow, TableCell, Paper, Button } from '@mui/material';
 import axios from 'axios';
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function ProdutoVincular(props) {
@@ -10,6 +10,7 @@ function ProdutoVincular(props) {
     const [modelosDisponiveis, setModelosDisponiveis] = useState([]);
     const [equipamentosDisponiveis, setEquipamentosDisponiveis] = useState([]);
     const [filtro, setFiltro] = useState("");
+    const [equipamentoSelecionado, setEquipamentoSelecionado] = useState(null);
 
     useEffect(() => {
         axios.get("https://lp7vw2q19f.execute-api.us-east-1.amazonaws.com/listar-disponiveis")
@@ -23,16 +24,51 @@ function ProdutoVincular(props) {
             });
     }, []);
 
-    // Função para lidar com a alteração de modelo
     const handleModeloChange = (event) => {
         const selectedModelo = event.target.value;
         setModelo(selectedModelo);
+        setEquipamentoSelecionado(null); // Limpa o equipamento selecionado ao mudar o modelo
     };
 
-    // Função para lidar com a alteração do filtro de pesquisa
     const handleFiltroChange = (event) => {
-        const valorFiltro = event.target.value.toUpperCase(); // Converter para maiúsculas
+        const valorFiltro = event.target.value.toUpperCase();
         setFiltro(valorFiltro);
+    };
+
+    const handleRowClick = (equipamento) => {
+        setEquipamentoSelecionado(equipamento);
+    };
+
+    const handleNomeEquipamentoChange = (event) => {
+        setNomeEquipamento(event.target.value.toUpperCase());
+    };
+
+    const vincularProduto = () => {
+        if (!equipamentoSelecionado || !nomeEquipamento) {
+            toast.warn('Por favor, selecione um equipamento e preencha o nome');
+            return;
+        }
+
+        axios.post("https://lp7vw2q19f.execute-api.us-east-1.amazonaws.com/vincular-equipamento", {
+            nome: nomeEquipamento,
+            modelo: equipamentoSelecionado.modelo,
+            n_serie: equipamentoSelecionado.n_serie,
+            patrimonio: equipamentoSelecionado.patrimonio,
+            categoria: equipamentoSelecionado.categoria,
+            status: 'EM USO' // Defina o status como 'em_uso'
+        })
+            .then(response => {
+                toast.success("Equipamento cadastrado com sucesso!", {
+                    autoClose: 2000,
+                });
+                // Limpar campos ou fazer outras ações após o salvamento bem-sucedido
+                setEquipamentoSelecionado(null);
+                setNomeEquipamento("");
+            })
+            .catch(error => {
+                console.error('Erro ao cadastrar equipamento:', error);
+                toast.error("Erro ao cadastrar equipamento. Por favor, tente novamente mais tarde.");
+            });
     };
 
     return (
@@ -41,7 +77,6 @@ function ProdutoVincular(props) {
                 <CardContent>
                     <div style={{ fontSize: "18px", fontWeight: "bold" }}>{props.texto}</div>
                     <div style={{ display: "flex", flexDirection: "column" }}>
-
                         <FormControl style={{ marginTop: "10px" }} fullWidth>
                             <InputLabel id="modelo-label">Modelo</InputLabel>
                             <Select
@@ -74,7 +109,6 @@ function ProdutoVincular(props) {
                                             <TableCell>Modelo</TableCell>
                                             <TableCell>Numero de Série</TableCell>
                                             <TableCell>Patrimonio</TableCell>
-                                            {/* Adicione mais células para outras informações, se necessário */}
                                         </TableRow>
                                     </TableHead>
                                     <TableBody>
@@ -87,16 +121,29 @@ function ProdutoVincular(props) {
                                                 equipamento.patrimonio.includes(filtro)
                                             )
                                             .map((equipamento) => (
-                                                <TableRow key={equipamento.id}>
+                                                <TableRow key={equipamento.id} onClick={() => handleRowClick(equipamento)}>
                                                     <TableCell>{equipamento.modelo}</TableCell>
                                                     <TableCell>{equipamento.n_serie}</TableCell>
                                                     <TableCell>{equipamento.patrimonio}</TableCell>
-                                                    {/* Adicione mais células para outras informações, se necessário */}
                                                 </TableRow>
                                             ))}
                                     </TableBody>
                                 </Table>
                             </TableContainer>
+                        )}
+                        {equipamentoSelecionado && (
+                            <div style={{ marginTop: "20px", display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                                <TextField
+                                    value={nomeEquipamento}
+                                    onChange={handleNomeEquipamentoChange}
+                                    label="Nome do Equipamento"
+                                    variant="outlined"
+                                    style={{ width: '100%' }}
+                                />
+                                <Button variant="contained" color="primary" onClick={vincularProduto} style={{ marginLeft: "10px", width: '30%', marginTop: "20px" }}>
+                                    Salvar
+                                </Button>
+                            </div>
                         )}
                     </div>
                 </CardContent>
