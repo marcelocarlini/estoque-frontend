617bc1277c42227e4d652e099f0d1f3f90b73e8b
Mccortez <marcelocarlini@hotmail.com>
2024-04-09 02:09:35 -0300

    Incluso o retorno

--- src/components/produto/ProdutoListaVinculados.js
+++ src/components/produto/ProdutoListaVinculados.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, TextField } from '@mui/material';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography, TextField, Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
 import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
 import '../../partials/_produtolista.scss';
 
-function ProdutoLista(props) {
+function ProdutoListaVinculados(props) {
     const [rows, setRows] = useState([]);
     const [filtro, setFiltro] = useState("");
+    const [selectedRow, setSelectedRow] = useState(null);
+    const [confirmationDialogOpen, setConfirmationDialogOpen] = useState(false);
 
     useEffect(() => {
         axios.get("https://lp7vw2q19f.execute-api.us-east-1.amazonaws.com/listar-equipamentos-vinculados")
@@ -20,6 +23,39 @@ function ProdutoLista(props) {
     const handleFiltroChange = (event) => {
         const valorFiltro = event.target.value.toUpperCase();
         setFiltro(valorFiltro);
+    };
+
+    const handleRowClick = (row) => {
+        setSelectedRow(row);
+    };
+
+    const handleDesvincularClick = () => {
+        setConfirmationDialogOpen(true);
+    };
+
+    const handleConfirmDesvincular = () => {
+        if (!selectedRow) {
+            console.error('Nenhum equipamento selecionado para desvincular.');
+            return;
+        }
+        axios.delete("https://lp7vw2q19f.execute-api.us-east-1.amazonaws.com/desvincular-equipamento", {
+            data: { n_serie: selectedRow.n_serie }
+        })
+            .then(response => {
+                toast.success("Equipamento desvinculado com sucesso!", {
+                    autoClose: 2000,
+                });
+                setSelectedRow(null);
+                setConfirmationDialogOpen(false);
+            })
+            .catch(error => {
+                console.error('Erro ao desvincular equipamento:', error);
+                setConfirmationDialogOpen(false);
+            });
+    };
+
+    const handleCancelDesvincular = () => {
+        setConfirmationDialogOpen(false);
     };
 
     const filtroResultado = rows.filter(row =>
@@ -48,22 +84,38 @@ function ProdutoLista(props) {
                             <TableCell className="produto-tabela-header" align="right">Numero de Série</TableCell>
                             <TableCell className="produto-tabela-header" align="right">Patrimonio</TableCell>
                             <TableCell className="produto-tabela-header">Modelo</TableCell>
+                            <TableCell className="produto-tabela-header">Ações</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {filtroResultado.slice().reverse().map((row) => (
-                            <TableRow key={row.id}>
+                        {filtroResultado.map((row) => (
+                            <TableRow key={row.id} onClick={() => handleRowClick(row)} className={selectedRow === row ? 'selected-row' : ''}>
                                 <TableCell className="produto-item">{row.nome}</TableCell>
                                 <TableCell align="right" className="produto-item">{row.n_serie}</TableCell>
                                 <TableCell align="right" className="produto-item">{row.patrimonio.toUpperCase()}</TableCell>
                                 <TableCell align="right" className="produto-item">{row.modelo.toUpperCase()}</TableCell>
+                                <TableCell align="right" className="produto-item">
+                                    {selectedRow === row && (
+                                        <Button variant="contained" color="secondary" onClick={handleDesvincularClick}>Desvincular</Button>
+                                    )}
+                                </TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
                 </Table>
             </TableContainer>
+            <Dialog open={confirmationDialogOpen} onClose={handleCancelDesvincular}>
+                <DialogTitle>Desvincular Equipamento</DialogTitle>
+                <DialogContent>
+                    <Typography>Deseja realmente desvincular o equipamento?</Typography>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCancelDesvincular} color="primary">Não</Button>
+                    <Button onClick={handleConfirmDesvincular} color="primary" autoFocus>Sim</Button>
+                </DialogActions>
+            </Dialog>
         </div>
     );
 }
 
-export default ProdutoLista;
+export default ProdutoListaVinculados;
